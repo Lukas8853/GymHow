42d09d2b41c2540061a156f9058658785c2e5507
lukas.1107pvt <lukas.1107pvt@gmail.com>
2026-02-20 20:38:49 +0000

    sync

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxBMyrKyzHoP78HRqQPq7Yqj6FX5BiGmM",
+  authDomain: "gymhow-zavrsni.firebaseapp.com",
+  projectId: "gymhow-zavrsni",
+  storageBucket: "gymhow-zavrsni.firebasestorage.app",
+  messagingSenderId: "460005855586",
+  appId: "1:460005855586:web:273ff224f13f3ffc0edc3b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
