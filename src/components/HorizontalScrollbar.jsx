48ea950239c6f4cb8fbaf5e2da619a6b443ada6b
MislavS <mislav.serdar1@gmail.com>
2026-02-21 19:33:59 +0100

    Scroll through exercises
    PROVJERI API!!! Nesto ne radi ili mene blokira il smo nesto zeznuli

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,11 +1,34 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, {useContext} from "react";
+import { Box, Typography } from "@mui/material";
+import { ScrollManu, ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+
 
 import BodyPart from "./BodyPart";
 
+import RightArrowIcon from "../assets/icons/right-arrow.png";
+import LeftArrowIcon from "../assets/icons/left-arrow.png";
+
+const LeftArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={LeftArrowIcon} alt="left-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+  return (
+    <Typography onClick={() => scrollNext()} className="right-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <div>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item) => (
         <Box
           key={item.id || item}
@@ -20,7 +43,7 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
           />
         </Box>
       ))}
-    </div>
+    </ScrollMenu>
   );
 };
 
